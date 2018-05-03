3849ee396522591f1b72e0dc974ccb2e42c00cfe
takagotch <dyaccb@gmail.com>
2018-05-04 01:37:24 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- cmd.sh
+++ cmd.sh
@@ -9,4 +9,14 @@ mysql apache_log < access_log.sql
 mysql apache_log
 grant insert,create on apaceh_log.* to webserver@localhost identified by 'wwwpw';
 
+//
+cd ServerRoot
+mv logs/access_log logs/access_log.old
+bin/apachectl graceful
+cd logs
+../bin/split-logfile < access_log.old
+
+//
+
+
 
