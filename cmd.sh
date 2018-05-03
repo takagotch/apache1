d1dc294ce4db16b26ff235906be762e2c00da2fb
takagotch <dyaccb@gmail.com>
2018-05-04 01:28:03 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- cmd.sh
+++ cmd.sh
@@ -4,3 +4,9 @@ chmod o+rx /home/bob/public_html
 find /home/bob/public_html -type f | xargs chmod 644
 
 
+mysqladmin create apache_log
+mysql apache_log < access_log.sql
+mysql apache_log
+grant insert,create on apaceh_log.* to webserver@localhost identified by 'wwwpw';
+
+
