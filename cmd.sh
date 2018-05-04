094136e804aae922246ffd915ee83a0a8aca2284
takagotch <dyaccb@gmail.com>
2018-05-04 09:03:30 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- cmd.sh
+++ cmd.sh
@@ -22,6 +22,43 @@ man -M ServerRoot/man/logresolve.8
 //
 man -M ServerRoot/man rotatelogs.8
 
+/path/to/apache/bin/apxs -cia module.c
+
+cd mod dav-1.0.3-1.3.6
+./configure --with-apxs=/usr/local/apache/bin/apxs
+make 
+make install
+
+cd /usr/local/apache
+mkdir var
+chgrp nobody var
+chmod g+w var
+
+cd /usr/local/apache
+mkdir htdocs/dav-test
+chgrp nobody htdocs/dav-test
+chmod g+w htdocs/dav-test
+
+cd /tmp
+echo "txt" > dav-test.txt
+cadaver
+open http://localhost/dav-test
+put dav-test.txt
+propset dav-test.txt MyProp 1023
+propget dav-test.txt MyProp
+propdel dav-test.txt MyProp
+close
+exit
+rm dav-test.txt
+
+
+cd mod_dav-1.0.3-dev
+copy mod_dav.dll C:\Apache\modules
+cd \Apache
+
+mkdir var
+mkdir htdocs\dav-test
+
 
 
 
